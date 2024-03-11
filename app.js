4f333d726a6e2834b736df7de2d8705f319e6dd1
Shreya Agrawal <shreya.agrawal2121@gmail.com>
2024-03-11 14:15:58 +0530

    GET and POST route created

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.post('/add_data', (req, res) =>{
+    console.log('Data added');
+    console.log(req.query.name);
+    console.log(req.body);
+    res.send('Data added');
+})
